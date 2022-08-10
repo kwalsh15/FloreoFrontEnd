70d994dce6563c1eb3eb8da63321db2cb228c68a
Enoc Castro <enocco70@gmail.com>
2022-08-09 19:44:15 -0600

    Creando la estructura del proyecto, utilizando Vite

---
+++ src/FloreoApp.jsx
@@ -0,0 +1,10 @@
+
+
+export const FloreoApp = () => {
+  
+    return (
+    <>
+        <h1>FloreoApp</h1>
+    </>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { FloreoApp } from './FloreoApp';
+import './styles.css';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FloreoApp />
+  </React.StrictMode>
+);

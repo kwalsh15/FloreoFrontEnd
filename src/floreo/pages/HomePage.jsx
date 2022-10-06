7c6a7011ac6e5372fc5fe0274cade1d504d04b6e
Enoc Castro <enocco70@gmail.com>
2022-10-06 15:13:57 -0600

    fix navbar

--- src/floreo/pages/HomePage.jsx
+++ src/floreo/pages/HomePage.jsx
@@ -1,9 +1,8 @@
-import { ImageCarousel } from "../../ui/components";
 
 export const HomePage = () => {
   return (
     <>
-      <ImageCarousel images={['https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300', 'https://unsplash.it/2000/1250?image=689']} />
+    <h1>Hola mundo</h1>
     </>
   );
 }
--- src/floreo/routes/FloreoRoutes.jsx
+++ src/floreo/routes/FloreoRoutes.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { NavbarUi } from '../../ui/components';
+import { ImageCarousel, NavbarUi } from '../../ui/components';
 import { HomePage, GalleryPage, AboutPage } from '../';
 import { Footer } from '../../ui/components/Footer';
 
@@ -8,6 +8,8 @@ export const FloreoRoutes = () => {
     return (
         <>
             <NavbarUi />
+            <ImageCarousel images={['https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300', 'https://unsplash.it/2000/1250?image=689']} />
+
             
             <Routes>
             
